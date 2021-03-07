5bc7e38a560672c82eaf1f7d0b7ae8b1c6cfe812
Ege Çıtak <egecitak01@gmail.com>
2021-03-07 18:21:04 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //A class, which implements IEntity, is a database table
+    public interface IEntity
+    {
+    }
+}
